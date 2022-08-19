a3fc367bc9c69dfff39f63f5bee90c0df367c0f8
Yuanqing Wang <wangyq+gh@wangyq.net>
2022-08-19 14:54:32 -0400

    difussion

--- bronx/layers.py
+++ bronx/layers.py
@@ -3,6 +3,49 @@ import torch
 
 class BronxLayer(torch.nn.Module):
     def __init__(
+        self,
+        hidden_features=None,
+        num_heads=1,
+        activation=torch.nn.ELU(),
+    ):
+        super().__init__()
+        self.fc_k = torch.nn.Linear(hidden_features, hidden_features, bias=False)
+        self.fc_q = torch.nn.Linear(hidden_features, hidden_features, bias=False)
+        self.fc = torch.nn.Linear(hidden_features, hidden_features, bias=False)
+        self.norm = torch.nn.LayerNorm(hidden_features)
+        self.activation = activation
+
+    def forward(self, h, x):
+        h = self.norm(h)
+        _x = x = torch.nn.functional.normalize(x, p=1, dim=-1)
+        xs = [_x]
+        for _ in range(self.num_heads-1):
+            _x = x @ _x
+            xs.append(_x)
+        x = torch.stack(xs, -1)
+
+        k = self.fc_k(h)
+        q = self.fc_q(h)
+
+        # (n, d, n_heads)
+        k = k.reshape(k.shape[0], int(self.hidden_features / self.num_heads), self.num_heads)
+        q = q.reshape(q.shape[0], int(self.hidden_features / self.num_heads), self.num_heads)
+
+        # (n, n, n_heads)
+        a = torch.einsum("xyb,zyb->xzb", k, q)
+        a = a.softmax(-2)
+
+        a = (a * x).sum(-1).softmax(-1)
+        h = a @ h
+        h = self.fc(h)
+        h = self.activation(h)
+        return h, x
+
+
+
+
+class BronxLayer(torch.nn.Module):
+    def __init__(
             self,
             in_features,
             hidden_features=None,
@@ -35,7 +78,7 @@ class BronxLayer(torch.nn.Module):
             torch.nn.Dropout(fc_drop),
             activation,
         )
-        
+
         # mixing
         mixing = torch.ones(num_heads + 1, num_heads +1) * semantic_weight
         mixing[0] = 0.0
@@ -82,7 +125,7 @@ class BronxLayer(torch.nn.Module):
         a_x = self.a_x_drop(a_x)
         v = self.fc_v(h)
         v = v.reshape(v.shape[0], int(self.out_features / self.num_heads), self.num_heads)
-        
+
         a = torch.cat([a_x.unsqueeze(-1), a_h], dim=-1)
         a = a @ self.mixing.softmax(0)
         a = torch.nn.functional.normalize(a, p=1, dim=-2)
