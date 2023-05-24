1dbd0dd274f2f236f6bf7dc360209b547173ab3f
Yuanqing Wang <wangyq@wangyq.net>
2023-05-24 03:31:18 -0400

    multihead

--- bronx/layers.py
+++ bronx/layers.py
@@ -9,43 +9,55 @@ from dgl import function as fn
 from dgl.nn.functional import edge_softmax
 
 def linear_diffusion(g, h, e=None, gamma=0.1):
-    a = g.adj()
-    if e is not None:
-        a.val.mul_(e)
-    a = a.to_dense()
-    a.fill_diagonal_(gamma)
+    a = g.adj().to_dense().unsqueeze(-1).repeat(1, 1, e.shape[-1])
+    a[g.edges()] = e
+    a = a.swapaxes(-1, 0)
+    a[
+        torch.eye(
+            a.shape[-1], device=a.device
+        ).bool().unsqueeze(0).repeat(e.shape[-1], 1, 1)
+    ] = gamma
     a = a / a.sum(-1, keepdims=True)
     a = torch.linalg.matrix_exp(a)
-    return a @ h
+    h = a @ h
+    h = h.mean(0)
+    return h
 
 class BronxLayer(pyro.nn.PyroModule):
     def __init__(
             self, 
             in_features, out_features, activation=torch.nn.SiLU(), 
-            dropout=0.0,
-            idx=0,
+            dropout=0.0, idx=0, num_heads=4,
         ):
         super().__init__()
         self.fc_k = torch.nn.Linear(in_features, out_features, bias=False)
         self.fc_mu = torch.nn.Linear(in_features, out_features, bias=False)
         self.fc_log_sigma = torch.nn.Linear(in_features, out_features, bias=False)
+        self.fc_out = torch.nn.Linear(in_features * num_heads, in_features)
         self.activation = activation
         self.idx = idx
         self.out_features = out_features
+        self.num_heads = num_heads
         self.dropout = torch.nn.Dropout(dropout)
 
     def guide(self, g, h):
         k, mu, log_sigma = self.fc_k(h), self.fc_mu(h), self.fc_log_sigma(h)
+        k = k.reshape(k.shape[0], self.num_heads, -1)
+        mu = mu.reshape(mu.shape[0], self.num_heads, -1)
+        log_sigma = log_sigma.reshape(log_sigma.shape[0], self.num_heads, -1)
         g.ndata["k"], g.ndata["mu"], g.ndata["log_sigma"] = k, mu, log_sigma
         g.apply_edges(fn.u_dot_v("k", "mu", "mu"))
         g.apply_edges(fn.u_dot_v("k", "log_sigma", "log_sigma"))
-        with pyro.plate(f"edges{self.idx}", g.number_of_edges(), device=g.device):
-            e = pyro.sample(
-                    f"e{self.idx}", 
-                    pyro.distributions.Normal(
-                    g.edata["mu"], g.edata["log_sigma"].exp(),
-               ).to_event(1)
-            )
+
+        with pyro.plate(f"heads{self.idx}", self.num_heads, device=g.device):
+            with pyro.plate(f"edges{self.idx}", g.number_of_edges(), device=g.device):
+                e = pyro.sample(
+                        f"e{self.idx}", 
+                        pyro.distributions.Normal(
+                        g.edata["mu"], g.edata["log_sigma"].exp(),
+                    ).to_event(1)
+                )
+
         return e
 
     def mp(self, g, h, e):
@@ -55,14 +67,15 @@ class BronxLayer(pyro.nn.PyroModule):
         return h
 
     def forward(self, g, h):
-        with pyro.plate(f"edges{self.idx}", g.number_of_edges(), device=g.device):
-            e = pyro.sample(
-                    f"e{self.idx}", 
-                    pyro.distributions.Normal(
-                        torch.zeros(g.number_of_edges(), 1, device=g.device),
-                        torch.ones(g.number_of_edges(), 1, device=g.device),
-               ).to_event(1)
-            )
+        with pyro.plate(f"heads{self.idx}", self.num_heads, device=g.device):
+            with pyro.plate(f"edges{self.idx}", g.number_of_edges(), device=g.device):
+                e = pyro.sample(
+                        f"e{self.idx}", 
+                        pyro.distributions.Normal(
+                            torch.zeros(g.number_of_edges(), self.num_heads, 1, device=g.device),
+                            torch.ones(g.number_of_edges(), self.num_heads, 1, device=g.device),
+                    ).to_event(1)
+                )
 
         h = self.mp(g, h, e)
         h = self.dropout(h)
