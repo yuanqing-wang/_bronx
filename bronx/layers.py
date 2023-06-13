53140299b05f9623daca2b52bb3f33d455acc56e
Yuanqing Wang <wangyq@wangyq.net>
2023-06-12 22:27:55 -0400

    fixed

--- bronx/layers.py
+++ bronx/layers.py
@@ -34,23 +34,22 @@ class InLayer(torch.nn.Module):
 class BronxLayer(torch.nn.Module):
     def __init__(
             self, 
-            in_features, out_features, gamma=1.0, idx=0, dropout=0.5,
+            in_features, out_features, gamma=1.0, idx=0, dropout=0.1,
         ):
         super().__init__()
-        self.fc_mu = torch.nn.Linear(in_features, out_features, bias=False)
-        self.fc_log_sigma = torch.nn.Linear(in_features, out_features, bias=False)
-        self.fc = torch.nn.Linear(in_features, out_features, bias=False)
+        self.fc_mu = torch.nn.Linear(in_features, out_features // 2, bias=False)
+        self.fc_log_sigma = torch.nn.Linear(in_features, out_features // 2, bias=False)
+        self.fc = torch.nn.Linear(in_features, out_features // 2, bias=False)
         self.idx = idx
         self.out_features = out_features
         self.gamma = gamma
-        self.dropout = torch.nn.Dropout(dropout)
+        self.dropout = dropout
 
     def guide(self, g, h):
         pyro.module(f"fc_mu{self.idx}", self.fc_mu)
         pyro.module(f"fc_log_sigma{self.idx}", self.fc_log_sigma)
         pyro.module(f"fc{self.idx}", self.fc)
         h0 = self.fc(h)
-        h0 = self.dropout(h0)
         with pyro.plate(f"nodes{self.idx}", g.number_of_nodes(), device=g.device):
             with pyro.poutine.scale(None, scale=float(g.ndata["train_mask"].sum() / g.number_of_nodes())):
                 h = pyro.sample(
@@ -60,26 +59,27 @@ class BronxLayer(torch.nn.Module):
                             self.fc_log_sigma(h).exp(),
                         ).to_event(1),
                 )
-        h = h0 + h
         a = exp_adj(g, gamma=self.gamma)
+        h0 = h.broadcast_to(h.shape)
+        h = torch.cat([h0, h], -1)
         h = a @ h
         return h
 
     def forward(self, g, h):
         pyro.module(f"fc{self.idx}", self.fc)
         h0 = self.fc(h)
-        h0 = self.dropout(h0)
         with pyro.plate(f"nodes{self.idx}", g.number_of_nodes(), device=g.device):
             with pyro.poutine.scale(None, scale=float(g.ndata["train_mask"].sum() / g.number_of_nodes())):
                 h = pyro.sample(
                         f"h{self.idx}", 
                             pyro.distributions.Normal(
-                                torch.zeros(g.number_of_nodes(), self.out_features, device=g.device),
-                                torch.ones(g.number_of_nodes(), self.out_features, device=g.device),
+                                torch.zeros(g.number_of_nodes(), self.out_features // 2, device=g.device),
+                                torch.ones(g.number_of_nodes(), self.out_features // 2, device=g.device),
                             ).to_event(1),
                 )
-        h = h0 + h
         a = exp_adj(g, gamma=self.gamma)
+        h0 = h.broadcast_to(h.shape)
+        h = torch.cat([h0, h], -1)
         h = a @ h
         return h
 
