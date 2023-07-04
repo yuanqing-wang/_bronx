09ac2993c6cee81e96424c2560030e1f052a5964
Yuanqing Wang <wangyq@wangyq.net>
2023-07-04 00:09:10 -0400

    tuning

--- scripts/node_classification/production.py
+++ scripts/node_classification/production.py
@@ -4,7 +4,7 @@ from ray.tune import ExperimentAnalysis
 from run import run as run_experiment
 from tune import multiply_by_heads
 
-def run(path):
+def run(path, n_repeat=3):
     analysis = ExperimentAnalysis(path)
     trial = analysis.get_best_trial("_metric/accuracy", "max", "all")
     accuracy = trial.metric_analysis["_metric/accuracy"]["max"]
@@ -16,7 +16,17 @@ def run(path):
     config["num_samples"] = 16
     config["num_particles"] = 16
     config = SimpleNamespace(**config)  
-    accuracy, accuracy_te = run_experiment(config)
+
+    accuracies = []
+    accuracies_te = []
+
+    for _ in range(n_repeat):
+        accuracy, accuracy_te = run_experiment(config)
+        accuracies.append(accuracy)
+        accuracies_te.append(accuracy_te)
+
+    accuracy = sum(accuracies) / len(accuracies)
+    accuracy_te = sum(accuracies_te) / len(accuracies_te)
     print(accuracy, accuracy_te)
 
 if __name__ == "__main__":
